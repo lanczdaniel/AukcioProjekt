08c890793a9e530af6df4be014c6caf180927075
lanczdaniel <56674783+lanczdaniel@users.noreply.github.com>
2019-10-21 09:57:29 +0200

    Kezdetek

---
+++ AukcioProjekt/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AukcioProjekt
+{
+    class Festmeny
+    {
+        private string cim;
+        private string festo;
+        private string stilus;
+        private int licitekSzama = 0;
+        private int legmagasabbLicit = 100;
+        private DateTime legmagasabbLicitIdeje;
+        private bool elkelt = false;
+
+        public Festmeny(string cim, string festmeny, string stilus)
+        {
+
+        }
+
+        public string getFesto()
+        {
+            return "semmi";
+        }
+
+        public string getStilus()
+        {
+            return "semmi";
+        }
+
+        public int getLicitekSzama()
+        {
+            return 0;
+        }
+
+        public int getLegmagasabbLicit()
+        {
+            return 0;
+        }
+
+        public bool Elkelt()
+        {
+            return false;
+        }
+
+        public Licit()
+        {
+            if ()
+            {
+
+            }
+            return;
+        }
+
+        public Licit(int mertek)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
